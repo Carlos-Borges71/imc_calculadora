4cebfa3229fed60a9751fc3d2e0265f357513636
Carlos Borges <kaca_master@hotmail.com>
2024-10-20 19:19:47 -0300

    Criado componente da calculadora

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ImcCalc from './components/ImcCalc'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="container">
+      <ImcCalc />
+     </div>
+    </>
+  )
+}
+
+export default App
