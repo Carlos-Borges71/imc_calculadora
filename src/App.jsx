57c4a2a43c8824541d3361ebed8924bcbd00f42a
Carlos Borges <kaca_master@hotmail.com>
2024-10-21 18:35:51 -0300

    Iniciando componente de tabela de informações

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import './App.css'
+import { data } from './data/data';
+
+import { useState } from 'react';
+
 import ImcCalc from './components/ImcCalc'
+import ImcTable from './components/ImcTable';
+
+import './App.css';
+
 
 function App() {
+
+  const calcImc = (e, height, weight) => {
+
+    e.preventDefault();
+    console.log("Executou");
+  }
+
+const [imc, setImc] = useState("");
+const [info, setInfo] = useState("");
+const [infoClass, setInfoClass] = useState("");
   
 
   return (
     <>
      <div className="container">
-      <ImcCalc />
+      {!imc ? <ImcCalc calcImc={calcImc}/> : <ImcTable />}
      </div>
     </>
   )
--- src/data/data.jsx
+++ src/data/data.jsx
@@ -21,6 +21,7 @@ export const data = [
       classification: "Entre 25,0 e 29,9",
       info: "Sobrepeso",
       obesity: "I",
+      infoClass: "good",
     },
     {
       min: 30,
@@ -28,6 +29,7 @@ export const data = [
       classification: "Entre 30,0 e 39,9",
       info: "Obesidade",
       obesity: "II",
+      infoClass: "good",
     },
     {
       min: 40,
@@ -35,5 +37,6 @@ export const data = [
       classification: "Maior que 40,0",
       info: "Obesidade grave",
       obesity: "III",
+      infoClass: "good",
     },
   ];
